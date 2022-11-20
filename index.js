a48777b00f33ff44c82ca346ed31b8195fb2b9e2
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-20 23:55:11 +0300

    event listeners

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let input = document.getElementById('button');
 function addingEventListener() {
+    alert('I was clicked!');
+    input.addEventListener('click', addingEventListener);
 }
+input.addEventListener('click', addingEventListener);
+
